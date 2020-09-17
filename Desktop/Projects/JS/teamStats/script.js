e7effe7efe1005323d08d0f5b50fa227094597d1
programmertaha <programmer.taha.1y@gmail.com>
2020-09-17 13:07:09 +0300

    initial comment

---
+++ Desktop/Projects/JS/teamStats/script.js
@@ -0,0 +1,12 @@
+/*
+
+create and extract information about your favorite sports team using data structures arrays and objects
+
+% manipulate these data structures
+% gain insights from them
+
+! get the total number of games your team has played
+! the average score of all of their games
+
+*/
+
