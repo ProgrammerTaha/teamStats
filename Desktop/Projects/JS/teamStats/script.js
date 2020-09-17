8ea56268abcf3dc0778c3f03761a999db9de2a7d
programmertaha <programmer.taha.1y@gmail.com>
2020-09-17 13:18:21 +0300

    #1-#2-#3-#4-#5 Tasks completed

--- Desktop/Projects/JS/teamStats/script.js
+++ Desktop/Projects/JS/teamStats/script.js
@@ -10,3 +10,43 @@ create and extract information about your favorite sports team using data struct
 
 */
 
+const team = {
+  _players: [
+    {
+      firstName: 'Taha',
+      lastName: 'Saad',
+      age: 24
+    }, {
+      firstName: 'Furqan',
+      lastName: 'Jamal',
+      age: 24
+    }, {
+      firstName: 'Ahmad',
+      lastName: 'Emara',
+      age: 35
+    }
+  ],
+  _games: [
+    {
+      opponent: 'Barshallona',
+      teamPoints: 42,
+      opponentPoints: 27
+    },
+    {
+      opponent: 'Riyal',
+      teamPoints: 50,
+      opponentPoints: 50
+    },
+    {
+      opponent: 'Saudi Arabia',
+      teamPoints: 20,
+      opponentPoints: 22
+    }
+  ],
+  get players() {
+    return this._players;
+  },
+  get games() {
+    return this._games;
+  },
+};
